4ad10904666b773dc737e7965a35e81352002208
DollStarTa2ist <98052300+DollStarTa2ist@users.noreply.github.com>
2023-02-27 15:03:08 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//new version - remixed from Jon's copy of party.....
+//Each file should contain a javadoc header with author, date/time, program name and purpose (5%)
+//Jonny Osman, Feb 27, Party Planner, Runs my pary class and imports the required files
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  //Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+
+  Party p1 = new Party();
+  p1.throwParty();
+
+  }
+}
