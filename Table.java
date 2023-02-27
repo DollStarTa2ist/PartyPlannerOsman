dab55aeadeacaa08bedb16cfe16d99f8ef70a0e7
DollStarTa2ist <98052300+DollStarTa2ist@users.noreply.github.com>
2023-02-27 15:04:24 -0500

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,10 @@
+//Jonny Osman, Feb 27, Party Planner, failed class attempt
+
+public class Table{
+public static void main (String [] arg){
+    //ArrayList<Table> table = new ArrayList<Table>();
+  
+  
+
+}
+}
